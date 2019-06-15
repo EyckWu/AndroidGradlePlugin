5f8417bfbda89d5221136995b2764e282989658e
WuGuiXin <EyckWu@gmail.com>
2019-06-15 18:08:38 +0800

    插件

--- gradleplugin/src/main/groovy/com/xtc/gradleplugin/HelloPlugin.groovy
+++ gradleplugin/src/main/groovy/com/xtc/gradleplugin/HelloPlugin.groovy
@@ -21,6 +21,29 @@ class HelloPlugin implements Plugin<Project> {
                 println 'hello, this is test task!'
             }
         }
+        // 所有模块都配置完了之后执行
+        project.afterEvaluate {
+            if (!project.android){
+                throw new IllegalStateException("Must apply 'com.android.application' or 'com.android.library' first!")
+            }
+
+            if (project.apkdistconf.nameMap == null || project.apkdistconf.destDir == null){
+                project.logger.info('Apkdist conf should be set!')
+                return
+            }
+
+            Closure nameMap = project['apkdistconf'].nameMap
+            String destDir = project['apkdistconf'].destDir
+
+            // 枚举每一个build variant,生成的apk 放入如下路径和文件名
+            project.android.applicationVariants.all {
+                variant.outputs.each { output ->
+                    File file = output.outputFile
+                    println("dir " + destDir + " " + file.getName())
+                    output.outputFile = new File(destDir, nameMap(file.getName()))
+                }
+            }
+        }
 
     }
 }
